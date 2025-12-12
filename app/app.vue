db095379c7340fcf24ef29fdbcf5abc44b16413d
Mattia <giacchini.mattia@gmail.com>
2025-12-12 14:44:58 +0100

    Basic logo for WIP

--- app/app.vue
+++ app/app.vue
@@ -7,9 +7,26 @@ useSeoMeta({
 </script>
 
 <template>
-  <div>
-    <AppHeader />
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="background">
+    <img src="./logo.svg" alt="Logo" class="logo"/>
   </div>
 </template>
+
+<style lang="scss">
+.background {
+  background-color: #FFEBBE; // el color de fondo
+  display: flex;
+  justify-content: center;   // centra horizontal
+  align-items: center;       // centra vertical
+  height: 100vh;             // ocupa toda la altura de la ventana
+  max-height: 100vh;             // ocupa toda la altura de la ventana
+  padding: 0 20%;            // margen lateral 20%
+  box-sizing: border-box;
+}
+
+.logo {
+  width: 100%;               // ocupará el ancho disponible dentro del padding
+  height: auto;              // mantiene ratio original
+  max-width: 100%;           // nunca excede del contenedor
+}
+</style>
