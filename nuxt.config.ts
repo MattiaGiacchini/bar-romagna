5ca2c99c5d6dc222f2bc5baac3e9027456f03518
Mattia <giacchini.mattia@gmail.com>
2025-12-13 17:54:42 +0100

    Posthog installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@
 import Lara from '@primeuix/themes/lara';
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      posthogPublicKey: 'phc_Bdmz4pGPXd4RfIQj9NMs41pzbAL5NxNhLN9vBiiMGpl',
+      posthogHost: 'https://eu.i.posthog.com',
+      posthogDefaults: '2025-11-30'
+    }
+  },
+
   // css: ['~/assets/scss/theme.scss'],
   compatibilityDate: '2026-01-01',
   devtools: { enabled: true },
